6cac6280b4a257d5b2952073c10946f06bd3a9fb
tyler hastings <thastings3@gatech.edu>
2013-03-03 15:06:03 -0500

    added tabs

---
+++ src/com/gatech/spark/fragment/WhatsHotfragment.java
@@ -0,0 +1,24 @@
+package com.gatech.spark.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.gatech.spark.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tyler
+ * To change this template use File | Settings | File Templates.
+ */
+public class WhatsHotfragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_section_dummy, container, false);
+        ((TextView) rootView.findViewById(android.R.id.text1)).setText( "Section" + "test");
+        return rootView;
+    }
+}
