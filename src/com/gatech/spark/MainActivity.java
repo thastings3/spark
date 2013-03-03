a2ce54cae853a0bd9efa853f03cbd193c9bc3a2e
Sam Britt <samuelbritt@gmail.com>
2013-03-03 15:02:19 -0500

    capture new lat/long on drag event

--- src/com/gatech/spark/MainActivity.java
+++ src/com/gatech/spark/MainActivity.java
@@ -1,17 +1,22 @@
 package com.gatech.spark;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
 {
     private GoogleMap map;
+    private Marker marker;
+    private static final LatLng GT = new LatLng(33.78102,-84.400363);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,7 +24,31 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.big_map);
         setUpMapIfNeeded();
-        addMarker(33.78102,-84.400363);
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(GT));
+        map.animateCamera(CameraUpdateFactory.zoomTo(12));
+        
+        marker = addMarker(GT);
+		marker.setDraggable(true);
+		map.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+			@Override
+			public void onMarkerDragStart(Marker arg0) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void onMarkerDragEnd(Marker marker) {
+				LatLng latlng = marker.getPosition();
+				Toast.makeText(MainActivity.this, "new position is " + latlng,
+						Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onMarkerDrag(Marker arg0) {
+				// TODO Auto-generated method stub
+			}
+		});
     }
     
     @Override
@@ -28,10 +57,13 @@ public class MainActivity extends Activity
     	setUpMapIfNeeded();
     }
     
-    private void addMarker(double lat, double lng) {
-        map.addMarker(new MarkerOptions()
-        .position(new LatLng(lat, lng))
-        .title("Hello world"));
+    private Marker addMarker(LatLng position) {
+        Marker marker = map.addMarker(new MarkerOptions()
+        .position(position)
+        .title("Hello world")
+        .snippet("Load: 14%")
+        .draggable(true));
+        return marker;
     }
 
 
