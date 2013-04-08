14cfd63a3e5c3fba00216bb3af017213d4af7368
Sam Britt <samuelbritt@gmail.com>
2013-04-08 09:44:20 -0400

    remove unused code

--- src/com/gatech/spark/helper/MarkerPlacer.java
+++ src/com/gatech/spark/helper/MarkerPlacer.java
@@ -1,8 +1,6 @@
 package com.gatech.spark.helper;
 
-import com.gatech.spark.R;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
